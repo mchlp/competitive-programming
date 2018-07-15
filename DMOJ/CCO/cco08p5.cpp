e81ac069db5764fbe43ed2c44d6244fcaa081fe1
Michael Pu <michael.pu123@gmail.com>
2018-07-14 21:16:53 -0400

    wip 2018-07-14

--- DMOJ/CCO/cco08p5.cpp
+++ DMOJ/CCO/cco08p5.cpp
@@ -4,11 +4,12 @@
 
 using namespace std;
 
-#define MAXN 101
+#define MAXN 5000000
+#define MAXC 201
 
-int candy[201];
+int candy[MAXC];
 
-int dp[]
+int dp[MAXN];
 
 int main() {
 
@@ -16,17 +17,35 @@ int main() {
 
     int N;
     int tCal = 0;
+    int maxC = 0;
 
     scanf("%d", &N);
 
     for (int i=0; i<N; i++) {
         int K, C;
         scanf("%d %d", &K, &C);
-        candy[c] += K;
+        candy[C] += K;
         tCal += C * K;
+        maxC = max(C, maxC);
     }
 
     int target = tCal/2;
+
+    for (int i=0; i<MAXN; i++) {
+        dp[i] = target;
+    }
+
+    for (int i=0; i<maxC; i++) {
+        for (int j=target; j>=0; j--) {
+            if (candy[i] > 0) {
+                for (int k=1; k<=min(candy[i], j-1); k++) {
+                    dp[j] = min(dp[j], ((dp[j-k] - candy[i]*k)+target)%target);
+                }
+            }
+        }
+    }
+
+    printf("%d %d\n", dp[target], target);
 
     return 0;
 }
