5dfd0d42458003465a40afc90c18e69277d1d96e
Michael Pu <michael.pu123@gmail.com>
2019-03-15 11:55:54 -0400

    Add more questions

--- DMOJ/ECOO/ecoo14r1p3.cpp
+++ DMOJ/ECOO/ecoo14r1p3.cpp
@@ -6,125 +6,136 @@ char words[31][31];
 bool marked[31][31];
 int R, C, M;
 vector<string> clues;
+vector<string> notfound;
 
 
 int main() {
 
-    scanf("%d%d", &R, &C);
-    for (int i = 0; i < R; i++) {
+    int _ = 5;
+    while (_--) {
+        clues.clear();
+        notfound.clear();
         memset(marked, false, sizeof marked);
-        for (int j = 0; j < C; j++) {
-            char c;
-            scanf(" %c", &c);
-            words[i][j] = c;
+
+        scanf("%d%d", &R, &C);
+        for (int i = 0; i < R; i++) {
+            for (int j = 0; j < C; j++) {
+                char c;
+                scanf(" %c", &c);
+                words[j][i] = c;
+            }
         }
-    }
 
-    scanf("%d", &M);
-    for (int i = 0; i < M; i++) {
-        string s;
-        cin >> s;
-        s.erase(remove_if(s.begin(), s.end(), [](char c) { return !isalpha(c); } ), s.end());
-        clues.push_back(s);
-    }
+        scanf("%d\n", &M);
+        for (int i = 0; i < M; i++) {
+            string s;
+            getline(cin, s);
+            s.erase(remove_if(s.begin(), s.end(), [](char c) { return !isalpha(c); }), s.end());
+            clues.push_back(s);
+        }
 
-    for (string clue : clues) {
-        bool nextWord = false;
-        for (int i = 0; i < C; i++) {
-            for (int j = 0; j < R; j++) {
-                int tries = 2;
-                while (tries--) {
-                    // across
-                    bool good = true;
-                    for (int k = 0; k < clue.size(); k++) {
-                        if (i + k > C - 1) {
-                            good = false;
-                            break;
-                        }
-                        if (clue[i + k] != words[i + k][j]) {
-                            good = false;
-                            break;
-                        }
-                    }
-                    if (good) {
+        for (string clue : clues) {
+            bool nextWord = false;
+            for (int i = 0; i < C; i++) {
+                for (int j = 0; j < R; j++) {
+                    int tries = 2;
+                    while (tries--) {
+                        // across
+                        bool good = true;
                         for (int k = 0; k < clue.size(); k++) {
-                            marked[i + k][j] = true;
+                            if (i + k > C - 1) {
+                                good = false;
+                                break;
+                            }
+                            if (clue[k] != words[i + k][j]) {
+                                good = false;
+                                break;
+                            }
                         }
-                        nextWord = true;
-                    }
+                        if (good) {
+                            for (int k = 0; k < clue.size(); k++) {
+                                marked[i + k][j] = true;
+                            }
+                            nextWord = true;
+                        }
 
-                    // vertical
-                    good = true;
-                    for (int k = 0; k < clue.size(); k++) {
-                        if (j + k > R - 1) {
-                            good = false;
-                            break;
-                        }
-                        if (clue[j + k] != words[i][j + k]) {
-                            good = false;
-                            break;
-                        }
-                    }
-                    if (good) {
+                        // vertical
+                        good = true;
                         for (int k = 0; k < clue.size(); k++) {
-                            marked[i][j + k] = true;
+                            if (j + k > R - 1) {
+                                good = false;
+                                break;
+                            }
+                            if (clue[k] != words[i][j + k]) {
+                                good = false;
+                                break;
+                            }
                         }
-                        nextWord = true;
-                    }
+                        if (good) {
+                            for (int k = 0; k < clue.size(); k++) {
+                                marked[i][j + k] = true;
+                            }
+                            nextWord = true;
+                        }
 
-                    // diagonal 1
-                    good = true;
-                    for (int k = 0; k < clue.size(); k++) {
-                        if (j + k > R - 1 || i + k > C - 1) {
-                            good = false;
-                            break;
-                        }
-                        if (clue[j + k] != words[i + k][j + k]) {
-                            good = false;
-                            break;
-                        }
-                    }
-                    if (good) {
+                        // diagonal 1
+                        good = true;
                         for (int k = 0; k < clue.size(); k++) {
-                            marked[i + k][j + k] = true;
+                            if (j + k > R - 1 || i + k > C - 1) {
+                                good = false;
+                                break;
+                            }
+                            if (clue[k] != words[i + k][j + k]) {
+                                good = false;
+                                break;
+                            }
                         }
-                        nextWord = true;
-                    }
+                        if (good) {
+                            for (int k = 0; k < clue.size(); k++) {
+                                marked[i + k][j + k] = true;
+                            }
+                            nextWord = true;
+                        }
 
-                    // diagonal 2
-                    good = true;
-                    for (int k = 0; k < clue.size(); k++) {
-                        if (j + k > R - 1 || i - k < 0) {
-                            good = false;
-                            break;
-                        }
-                        if (clue[j + k] != words[i - k][j + k]) {
-                            good = false;
-                            break;
-                        }
-                    }
-                    if (good) {
+                        // diagonal 2
+                        good = true;
                         for (int k = 0; k < clue.size(); k++) {
-                            marked[i - k][j + k] = true;
+                            if (j + k > R - 1 || i - k < 0) {
+                                good = false;
+                                break;
+                            }
+                            if (clue[k] != words[i - k][j + k]) {
+                                good = false;
+                                break;
+                            }
                         }
-                        nextWord = true;
-                    }
+                        if (good) {
+                            for (int k = 0; k < clue.size(); k++) {
+                                marked[i - k][j + k] = true;
+                            }
+                            nextWord = true;
+                        }
 
-                    reverse(clue.begin(), clue.end());
+                        reverse(clue.begin(), clue.end());
+                    }
+                }
+                if (nextWord) {
+                    break;
                 }
             }
-            if (nextWord) {
-                break;
+            if (!nextWord) {
+                notfound.push_back(clue);
             }
         }
-    }
 
-    for (int i = 0; i < C; i++) {
-        for (int j = 0; j < R; j++) {
-            if (!marked[i][j]) {
-                printf("%c", words[i][j]);
+        for (int i = 0; i < C; i++) {
+            for (int j = 0; j < R; j++) {
+                if (!marked[j][i]) {
+                    printf("%c", words[j][i]);
+                }
             }
         }
+        printf("\n");
     }
     return 0;
 }
